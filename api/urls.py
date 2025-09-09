ffc44c69066e7bfe2861bffef7cbb7f934f296c5
Your Name <you@example.com>
2025-09-10 00:52:40 +0300

    добавил в api возмождность увеличивать/уменьшать счетчики( только для залогиненых и только свои), так же добавил возможность создавать, не придумал как сделать post запрос по другому, так что добавил method POST на форму (в файл conter.html)

--- api/urls.py
+++ api/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('api/counters',views.CounterListView.as_view(),name='counter_list_serialize'),
     path('api/counters/<pk>/',views.CounterDetailView.as_view(),name='counter_detail_serialize'),
     path('api/increase/<pk>/',views.CounterIncreaseView.as_view(),name='counter_increase'),
+    path('api/decrease/<pk>/',views.CounterDecreaseView.as_view(),name='counter_decrease'),
+    path('api/new_counter/',views.NewCounter.as_view(),name='new_counter'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from app_counter.serializers import CounterSerializer
 from app_counter.models import Counter
 from django.shortcuts import get_object_or_404
-
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import F
 
 
 class HelloAPIView(APIView):
@@ -26,13 +27,42 @@ class CounterDetailView(generics.RetrieveAPIView):
     queryset = Counter.objects.all()
     serializer_class = CounterSerializer
 
+
 class CounterIncreaseView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
     def get(self,request,pk):
-        counter = get_object_or_404(Counter,pk=pk)
+        counter = get_object_or_404(Counter,pk=pk,user=request.user)
         counter.value +=1
         counter.save()
 
         return Response({
             'action': 'increase',
+            'isComplete': True,
+        })
+
+class CounterDecreaseView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request,pk):
+        counter = get_object_or_404(Counter,pk=pk,user=request.user)
+        counter.value -=1
+        counter.save()
+
+        return Response({
+            'action': 'decrease',
+            'isComplete': True,
+        })
+
+class NewCounter(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        counter = Counter.objects.create(user=request.user)
+
+        return Response({
+            'action': 'new counter',
             'isComplete': True,
         })
